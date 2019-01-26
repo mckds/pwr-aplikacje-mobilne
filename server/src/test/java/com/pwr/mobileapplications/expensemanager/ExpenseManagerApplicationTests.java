d5c15cf97832e604784c9688667c762aec664b7c
mckds <dadosmaciej@gmail.com>
2019-01-26 21:59:14 +0100

    initialized server project

---
+++ server/src/test/java/com/pwr/mobileapplications/expensemanager/ExpenseManagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pwr.mobileapplications.expensemanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExpenseManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
