434059dbe667f2a355fe77795dda983f6269bdd5
mckds <dadosmaciej@gmail.com>
2019-01-31 02:55:52 +0100

    Added authentication service, menu and stubs for other components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,18 +6,35 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { LoginComponent } from './login/login.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { JwtInterceptor } from './interceptor/jwt.interceptor';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { BudgetComponent } from './budget/budget.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { UsersComponent } from './users/users.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent,
+    HomeComponent,
+    BudgetComponent,
+    CategoriesComponent,
+    ExpensesComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit() {
+    if(this.isAlreadyLoggedIn()) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  async onSignIn(username: string, password: string) {
+    const result = await this.authService.login(username, password);
+    if(result) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  async onSignUp(username: string, password: string) {
+    const result = await this.authService.register(username, password);
+    if(result) {
+      this.router.navigate(['/']);
+    }
+  }
+
+
+  isAlreadyLoggedIn() {
+    return localStorage.getItem("currentUser");
   }
 
 }
