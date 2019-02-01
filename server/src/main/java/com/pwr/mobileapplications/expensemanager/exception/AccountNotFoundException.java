aaff2e00fd9eb8f3fa95f4785868d955652e78be
Carlloy <karolinazdon@gmail.com>
2019-02-01 17:45:54 +0100

    Added http statuses to exceptions

--- server/src/main/java/com/pwr/mobileapplications/expensemanager/exception/AccountNotFoundException.java
+++ server/src/main/java/com/pwr/mobileapplications/expensemanager/exception/AccountNotFoundException.java
@@ -1,5 +1,9 @@
 package com.pwr.mobileapplications.expensemanager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class AccountNotFoundException extends RuntimeException {
     public AccountNotFoundException(String message) {
         super(message);
